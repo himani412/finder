28cd6371eed6c9795f2f63cd7ffbddd837577ccb
himani412 <77782996+himani412@users.noreply.github.com>
2021-09-07 00:43:56 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 function setup(){
     canvas=createCanvas(480,380);
     canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
     
+}
+function start(){
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects ";
+    document.getElementById("objectname").value;
+}
+function modelLoaded(){
+    console.log("ModelLoaded !");
+    status = true;
+}
+function draw(){
+    image(video,0,0,480,380);
 }
